edc6f23fe0c7f40b007cc1e06bdc06660c5f9dd6
max8teen <ggmaxwell007@gmail.com>
2026-05-30 10:17:08 +0530

    Update SiteNav.tsx

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -47,7 +47,7 @@ export function SiteNav() {
             <img
               src={logo}
               alt="Café Eternal"
-              className="h-13 w-13 object-contain transition-all duration-300"
+              className="h-12 w-12 object-contain transition-all duration-300"
               style={{
                 filter: solid ? 'none' : 'brightness(0) invert(1)',
               }}
